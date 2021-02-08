e79c4884cd74cb540517a225825b1a6abfabebe8
Emmanuel <qamanny@academytrainee.com>
2021-02-08 15:27:49 +0000

    Beginning Annotations

--- src/main/java/com/qa/demosite/Demo.java
+++ src/main/java/com/qa/demosite/Demo.java
@@ -1,8 +1,29 @@
 package com.qa.demosite;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.After;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class Demo {
 	
 	public static RemoteWebDriver driver;
 	private static final String URL = "http://thedemosite.co.uk/";
+	
+	  @Before
+	    public void setup() {
+	        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chrome/chromedriver.exe");
+	        driver = new ChromeDriver();
+	        driver.manage().window().setSize(new Dimension(1366, 768));
+	    }
+	  
+	  @Test
+	  
+	  
+	  @After
+	    public void tearDown() {
+	        driver.close();
+	    }
 
 }
